7f49685321a81afccde484ce84d9caa1a371e965
kenykeczuoquoi <72889148+kenykeczuoquoi@users.noreply.github.com>
2020-11-06 22:45:52 +0100

    Add files via upload

---
+++ DP.Notifications/Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using DP.Notifications.Model;
+using DP.Notifications.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DP.Notifications.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public void SendMessage(EmailMessageRequest request)
+        {
+            EmailSender sender = new EmailSender();
+
+            sender.SendNewUserEmail(request.EmailAddress);
+        }
+    }
+}
